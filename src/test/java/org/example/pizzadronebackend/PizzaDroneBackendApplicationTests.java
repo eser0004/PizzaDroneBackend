cd5ae98ab9abb854139427d94b12e9c7d0c8c366
DekaSkole <eser0004@stud.kea.dk>
2025-01-16 10:31:37 +0100

    first commit

---
+++ src/test/java/org/example/pizzadronebackend/PizzaDroneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pizzadronebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDroneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
